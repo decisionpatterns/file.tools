4767460fc97d95a9c0679ad1ec0c2bf018547787
Christopher Brown <cbrown@decisionpatterns.com>
2017-10-19 14:55:25 -0700

    v0.2.0 - add mkdir as alias for dir.create

---
+++ R/mkdir.R
@@ -0,0 +1,9 @@
+#' mkdir - alias for dir.create
+#'
+#' A friendly alias for dir.create
+#'
+#' @param ... arguments passed to base::dir.create()
+#'
+#' @export
+
+mkdir <- function(...) dir.create(...)
--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 
+ - [ ] move optigrab::this_file to here
  - [ ] Make `file_mtime` respect system time zone
 
